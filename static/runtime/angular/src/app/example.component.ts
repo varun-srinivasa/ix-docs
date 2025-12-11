481d15f853f90351cc7fd2729f1fa5a7626c14aa
varun srinivasa <srinivasa-varun.vanamamulai@siemens.com>
2025-12-11 09:02:44 +0530

    updated component.ts files

--- static/runtime/angular/src/app/example.component.ts
+++ static/runtime/angular/src/app/example.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
+  standalone: false,
 })
 export default class ExampleComponent {}
